4a2748e2740b3fcf09cdf6c89ab38abf9af6e520
Felix Michel <felix_michel@gmx.ch>
2016-11-15 15:23:19 +0100

    Update 2016-11-15T14:23:17.041Z

--- app/js/index.js
+++
@@ -1,8 +0,0 @@
-import { mount } from 'riot'
-
-import './components/sealevel-map.tag'
-import './components/sealevel-app.tag'
-import './components/sealevel-details.tag'
-import './components/sealevel-linechart.tag'
-
-export { mount }
